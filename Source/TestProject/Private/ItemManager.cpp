4676d7fd77404742d68f2a4fb5f96ff4542c246f
DevHN <devhn0@gmail.com>
2026-04-23 19:46:39 +0900

    Add log on ItemManager.cpp

--- Source/TestProject/Private/ItemManager.cpp
+++ Source/TestProject/Private/ItemManager.cpp
@@ -9,11 +9,15 @@ void UItemManager::InitializeItemDB()
 {
 	ItemDatabase.Add(101, FItemData(TEXT("Basic Potion"), TEXT("")));
 	ItemDatabase.Add(102, FItemData(TEXT("Master Sword"), TEXT("LINK")));
+
+	UE_LOG(LogTemp, Log, TEXT("→ Item Database Initialized. %d items added."), ItemDatabase.Num());
 }
 
 void UItemManager::AddTitle(FString NewTitle)
 {
 	AcquiredTitles.Add(NewTitle);
+	
+	UE_LOG(LogTemp, Log, TEXT("→ You obtained '%s' title!"), *NewTitle);
 }
 
 void UItemManager::ObtainItem(int32 ItemID)
@@ -21,12 +25,21 @@ void UItemManager::ObtainItem(int32 ItemID)
 	if (ItemDatabase.Contains(ItemID))
 	{
 		Bag.Add(ItemID);
+		
+		UE_LOG(LogTemp, Log, TEXT("→ You put your item (ID: %d) to your bag."), ItemID);
+	} else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("→ You tried to put ID(%d) which is not existed."), ItemID);
 	}
 }
 
 bool UItemManager::UseItem(int32 ItemID)
 {
-	if (!Bag.Contains(ItemID)) return false;
+	if (!Bag.Contains(ItemID))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("→ You don't have item (ID: %d)."), ItemID);
+		return false;
+	}
 	
 	FItemData* Info = ItemDatabase.Find(ItemID);
 	
@@ -35,7 +48,12 @@ bool UItemManager::UseItem(int32 ItemID)
 		if (Info->RequiredTitle.IsEmpty() || AcquiredTitles.Contains(Info->RequiredTitle))
 		{
 			Bag.RemoveSingle(ItemID);
+			UE_LOG(LogTemp, Log, TEXT("→ You successfully used %s."), *Info->ItemName);
 			return true;
+		} else
+		{
+			UE_LOG(LogTemp, Log, TEXT("→ You need %s title to use %s."), *Info->RequiredTitle, *Info->ItemName);
+			return false;
 		}
 	}
 	return false;
